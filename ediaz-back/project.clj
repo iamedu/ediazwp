c3bdaf7bcb048663ab35479a365f8012e18a1b88
Eduardo Diaz <iamedu@Gmail.com>
2013-07-29 06:08:05 -0500

    Agregando dependencias

--- ediaz-back/project.clj
+++ ediaz-back/project.clj
@@ -9,7 +9,17 @@
                  [com.taoensso/timbre "2.1.2"]
                  [com.postspectacular/rotor "0.1.0"]
                  [com.taoensso/tower "1.7.1"]
-                 [markdown-clj "0.9.28"]]
+                 [markdown-clj "0.9.28"]
+                 ;Custom dependencies
+                 [com.cemerick/friend "0.1.5"]
+                 [org.postgresql/postgresql "9.2-1003-jdbc4"]
+                 [clojurewerkz/mailer "1.0.0-alpha3"]
+                 [http-kit "2.1.8"]
+                 [ring/ring-json "0.2.0"]
+                 [ring-anti-forgery "0.2.1"]
+                 [clj-rss "0.1.3"]
+                 [com.novemberain/validateur "1.2.0"]
+                 [lobos "1.0.0-beta1"]]
   :plugins [[lein-ring "0.8.5"]]
   :ring {:handler ediaz-back.handler/war-handler
          :init    ediaz-back.handler/init
