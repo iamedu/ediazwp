9f750bf7100d540b624f4f17a00f16e3c334bae6
Eduardo Diaz <iamedu@Gmail.com>
2013-07-31 06:10:19 -0500

    Fixing data to be mixed

--- ediaz-back/src/ediaz_back/routes/home.clj
+++ ediaz-back/src/ediaz_back/routes/home.clj
@@ -4,12 +4,7 @@
             [ediaz-back.util :as util]))
 
 (defn home-page []
-  (layout/render
-    "home.html" {:content (util/md->html "/md/docs.md")}))
-
-(defn about-page []
-  (layout/render "about.html"))
+  (layout/render "index.html"))
 
 (defroutes home-routes
-  (GET "/" [] (home-page))
-  (GET "/about" [] (about-page)))
+  (GET "/" [] (home-page)))
